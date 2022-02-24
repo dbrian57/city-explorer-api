063ec6c604784a1d8ddeb8d08d05816ff42c1bee
Daniel Brian <daniel.brian@gmail.com>
2022-02-23 21:16:58 -0500

    added live weather and movie routes

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 'use strict'
 
-console.log('Hello world')
-
 const express = require('express');
 
 const app = express();
@@ -12,22 +10,44 @@ const PORT = process.env.PORT || 3001;
 const cors = require('cors');
 app.use(cors());
 
-let data = require('./data/weather.json');
+const axios = require('axios');
 
 app.get('/', (request, response) => {
-    response.send("Hello, from server!")
-    }
+    response.send("Hello, from server!")}
     );
 
-app.get('/weather', (request, response) => {
-    console.log(request.query);
+// Test URL http://localhost:3001/weather?searchQuery=Seattle&lon=-122.332069&lat=47.606209
+
+app.get('/weather', async (request, response) => {
     let lat = request.query.lat;
     let lon = request.query.lon;
     let searchQuery = request.query.searchQuery;
-    let weatherObject = data.find(city => city.city_name.toLowerCase() === searchQuery.toLowerCase());
-    let dataToSend = weatherObject.data.map(day => new Forcast(day.datetime, day.weather.description))
+    let url = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&units=I&days=3&lat=${lat}&lon=${lon}`;
+    
+    let weatherArray = [];
+    let getWeather = await axios.get(url);
+    weatherArray.push(getWeather);
+
+    let weatherObject = weatherArray.find(city => city.data.city_name.toLowerCase() === searchQuery.toLowerCase());
+    
+    let dataToSend = weatherObject.data.data.map(day => new Forcast(day.datetime, day.weather.description));
+
     response.send(dataToSend);
+
 })
+
+// Test URL http://localhost:3001/movies?searchQuery=Seattle
+
+app.get('/movies', async (request, response) => {
+  let searchQuery = request.query.searchQuery;
+  let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${searchQuery}`;
+
+  let getMovieObject = await axios.get(url);
+
+  let movieToSend = getMovieObject.data.results.map(movie => new Movies(movie.title, movie.release_date, movie.overview));
+  response.send(movieToSend);
+}
+);
 
 class Forcast { 
     constructor(date, description){
@@ -36,30 +56,19 @@ class Forcast {
     }
 }
 
-app.listen(PORT, (() => console.log(`Listening on port ${PORT}`)));
-
-/*
-app.get ('/sayHello', (request, response) => {
-  console.log(request.query);
-  let name = request.query.name;
-  response.send(`Hello ${name}`)
-})
-
-app.get('/pet', (request, response) => {
-  let species = request.query.species;
-  console.log(species);
-
-  
-  let dataToSend = data.find(element => element === species)
-  response.send(dataToSend);
-})
-
-class Pet {
-  constructor(petObject){
-    this.name = petObject.name;
-    this.breed = petObject.breed
+class Movies {
+  constructor(title, release_date, overview){
+    this.title = title;
+    this.release_date = release_date;
+    this. overview = overview;
   }
 }
 
-// npx kill-port 3001
+/*
+app.use((error, request, response, next) => {
+  console.log(error.message);
+  response.status(500).send(error.message);
+});
 */
+
+app.listen(PORT, (() => console.log(`Listening on port ${PORT}`)));
